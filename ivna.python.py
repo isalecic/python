9df9e6d3fabbe45936652375e4a7d67c437432f5
isalecic <50215668+isalecic@users.noreply.github.com>
2019-05-02 20:45:45 +0200

    Add files via upload

---
+++ ivna.python.py
@@ -0,0 +1,18 @@
+import math as m
+import random as r
+
+print('korijen broja 4:')
+korijen=m.sqrt(4)
+print(korijen)
+
+print('kvadrat broja 2:')
+kvadrat=m.pow(2,2)
+print(kvadrat)
+
+print('nasumičan broj:')
+nasumicanbr=r.random()
+print(nasumicanbr)
+
+import numpy as np
+matrica = np.array ([[2, 6, 1, 0], [1, 2, 0, 5], [9, 3, 4, 9]])
+print(matrica[:,1])
